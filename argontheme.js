e9e40db296ed5f0671a45cdf88e8a7b3a2aac446
yzfh <117617811+yzfh-ty@users.noreply.github.com>
2024-07-20 13:39:57 +0800

    Site updated: 2024-07-20 13:39:57

--- argontheme.js
+++ argontheme.js
@@ -1076,19 +1076,6 @@ function typeEffect(element, text, now, interval){
 	}
 }();
 
-/* 一言 */
-if ($(".hitokoto").length > 0){
-	$.ajax({
-		type: 'GET',
-		url: "https://v1.hitokoto.cn",
-		success: function(result){
-			$(".hitokoto").text(result.hitokoto);
-		},
-		error: function(result){
-			$(".hitokoto").text(__("Hitokoto 获取失败"));
-		}
-	});
-}
 
 /* Highlight.js */
 function randomString(len) {
